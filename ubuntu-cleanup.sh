d374b6d99af44538b85f58c7084c1f28348039e6
Chris McClanahan <mcclanc@gmail.com>
2019-02-24 17:30:05 -0600

    added cleanup script

---
+++ ubuntu-cleanup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Add usernames to add to /etc/sudoers for passwordless sudo
+users=("ubuntu" "admin")
+
+for user in "${users[@]}"
+do
+  cat /etc/sudoers | grep ^$user
+  RC=$?
+  if [ $RC != 0 ]; then
+    bash -c "echo \"$user ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers"
+  fi
+done
+
+#add VMware package keys
+# wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub -O - | apt-key add -
+# wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub -O - | apt-key add -
+
+#grab Ubuntu Codename
+codename="$(lsb_release -c | awk {'print $2}')"
+
+#add VMware repository to install open-vm-tools-deploypkg
+# echo "deb http://packages.vmware.com/packages/ubuntu $codename main" > /etc/apt/sources.list.d/vmware-tools.list
+
+#update apt-cache
+apt-get update
+
+#install packages
+# apt-get install -y open-vm-tools
+apt-get install -y python-minimal
+
+#Stop services for cleanup
+service rsyslog stop
+
+#clear audit logs
+if [ -f /var/log/audit/audit.log ]; then
+    cat /dev/null > /var/log/audit/audit.log
+fi
+if [ -f /var/log/wtmp ]; then
+    cat /dev/null > /var/log/wtmp
+fi
+if [ -f /var/log/lastlog ]; then
+    cat /dev/null > /var/log/lastlog
+fi
+
+#cleanup persistent udev rules
+if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
+    rm /etc/udev/rules.d/70-persistent-net.rules
+fi
+
+#cleanup /tmp directories
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+
+#cleanup current ssh keys
+rm -f /etc/ssh/ssh_host_*
+
+#add check for ssh keys on reboot...regenerate if neccessary
+sed -i -e 's|exit 0||' /etc/rc.local
+sed -i -e 's|.*test -f /etc/ssh/ssh_host_dsa_key.*||' /etc/rc.local
+bash -c 'echo "test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server" >> /etc/rc.local'
+bash -c 'echo "exit 0" >> /etc/rc.local'
+
+#reset hostname
+cat /dev/null > /etc/hostname
+
+#cleanup apt
+apt-get clean
+
+#cleanup shell history
+history -w
+history -c
+
+# Run
+cloud-init clean
+
+# Fix machine-id
+sudo truncate -s 0 /etc/machine-id
+sudo rm /var/lib/dbus/machine-id
+sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
